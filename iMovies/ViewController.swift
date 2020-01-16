03d10a3b584d819228e9e6360a9e23d722dc763a
Oumayma.ben-ghalba <oumayma.ben-ghalba@proxym-it.com>
2020-01-16 16:08:30 +0100

    first commit

---
+++ iMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMovies
+//
+//  Created by Oumeima ben ghalba on 1/16/20.
+//  Copyright © 2020 Oumeima ben ghalba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
